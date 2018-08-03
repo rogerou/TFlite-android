1e496d88ab43a2d367afcadeb5c0b21b039ad3b8
rogerou <379489343@qq.com>
2018-08-03 14:54:28 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -4,25 +4,25 @@ include $(CLEAR_VARS)
 
 
 SOURCES := \
-			$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/*.cc) \
-			$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/*.cc) \
-			$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/internal/*.cc) \
-			$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/internal/optimized/*.cc) \
-			$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/internal/reference/*.cc) \
-			$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/*.c) \
-			$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/*.c) \
-			$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/internal/*.c) \
-			$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/internal/optimized/*.c) \
-			$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/internal/reference/*.c) \
-			$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/downloads/farmhash/src/farmhash.cc)
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/*.cc) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/*.cc) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/internal/*.cc) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/internal/optimized/*.cc) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/internal/reference/*.cc) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/*.c) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/*.c) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/internal/*.c) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/internal/optimized/*.c) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/internal/reference/*.c) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/downloads/farmhash/src/farmhash.cc)
 
 CORE_CC_ALL_SRCS := $(sort $(SOURCES))
 CORE_CC_EXCLUDE_SRCS := \
-$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/*test.cc) \
-$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/*/*test.cc) \
-$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/*/*/*test.cc) \
-$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/*/*/*/*test.cc) \
-$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/test_util.cc) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/*test.cc) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/*/*test.cc) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/*/*/*test.cc) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/*/*/*/*test.cc) \
+		$(wildcard $(LOCAL_PATH)/include/tensorflow/contrib/lite/kernels/test_util.cc) \
 
 
 TF_LITE_CC_SRCS := $(filter-out $(CORE_CC_EXCLUDE_SRCS), $(CORE_CC_ALL_SRCS))
@@ -40,10 +40,10 @@ INCLUDES := \
 
 
 LDFLAGS := \
-			-Wl,--no-export-dynamic \
-			-Wl,--exclude-libs,ALL \
-			-Wl,--gc-sections \
-			-Wl,--as-needed
+		-Wl,--no-export-dynamic \
+		-Wl,--exclude-libs,ALL \
+		-Wl,--gc-sections \
+		-Wl,--as-needed
 	
 
 LDLIBS := \
@@ -56,27 +56,27 @@ CXXFLAGS := -std=c++11 -frtti
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 	CXXFLAGS+=		
-					-mfpu=neon-vfpv4 \
-					-mfloat-abi=softfp \
-					-funsafe-math-optimizations \
-					-ftree-vectorize
+			-mfpu=neon-vfpv4 \
+			-mfloat-abi=softfp \
+			-funsafe-math-optimizations \
+			-ftree-vectorize
 endif
 
 
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-	CXXFLAGS+=		-march=armv8-a\
-					-funsafe-math-optimizations \
-					-ftree-vectorize
+	CXXFLAGS+=	-march=armv8-a\
+			-funsafe-math-optimizations \
+			-ftree-vectorize
 						
 endif
 
 
 LOCAL_EXPORT_C_INCLUDES 	:= $(INCLUDES)
-LOCAL_C_INCLUDES 			+= $(INCLUDES)
-LOCAL_CPPFLAGS              := $(CXXFLAGS)
-LOCAL_SRC_FILES				:= $(TF_LITE_CC_SRCS)
-LOCAL_EXPORT_LDLIBS         := $(LDLIBS)
-LOCAL_ARM_MODE              := arm
+LOCAL_C_INCLUDES 		+= $(INCLUDES)
+LOCAL_CPPFLAGS              	:= $(CXXFLAGS)
+LOCAL_SRC_FILES			:= $(TF_LITE_CC_SRCS)
+LOCAL_EXPORT_LDLIBS         	:= $(LDLIBS)
+LOCAL_ARM_MODE             	:= arm
 LOCAL_MODULE := tensorflow-lite
 
 
